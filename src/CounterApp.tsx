ddb4f32964dd68415957572ddda4053762e8b53e
franciscomarin98 <francmarinc@gmail.com>
2021-11-10 14:44:49 -0500

    feat: React Fundamentals
    Practicing with handle events (click).

--- src/CounterApp.tsx
+++ src/CounterApp.tsx
@@ -1,11 +1,32 @@
 import PropTypes from "prop-types";
+import {useState} from "react";
 
 const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const handleIncrement = () => {
+        setCounter(counter + 1);
+    }
+
+    const handleReset = () => {
+        setCounter(0);
+    }
+
+    const handleDecrement = () => {
+        setCounter(counter - 1);
+    }
+
     return (
         <>
             <div className={"counter"}>
                 <h1>CounterApp</h1>
-                <h2> {value} </h2>
+                <h2> {counter} </h2>
+                <div className="counter-buttons">
+                    <button onClick={handleIncrement}>+1</button>
+                    <button onClick={handleReset}>Reset</button>
+                    <button onClick={handleDecrement}>-1</button>
+                </div>
             </div>
         </>
     );
