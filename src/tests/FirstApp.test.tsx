ded3bb755fcd68f3a47f9223ef6743c759b69e37
franciscomarin98 <francmarinc@gmail.com>
2021-11-14 12:58:52 -0500

    :white_check_mark:  feat: React Testing
    Migrating from react testing library to Enzyme.

--- src/tests/FirstApp.test.tsx
+++ src/tests/FirstApp.test.tsx
@@ -1,10 +1,16 @@
-import {render} from '@testing-library/react'
 import FirstApp from '../FirstApp'
+import {shallow} from 'enzyme';
 
 describe('Test in <FirstApp />', () => {
-    test('should show the title message: Hello World', () => {
+    /*    test('should show the title message: Hello World', () => {
+            const msg = 'Hello World';
+            const wrapper = render(<FirstApp title={msg}/>);
+            expect(wrapper.getByText(msg)).toBeInTheDocument();
+        })*/
+
+    test('should show the component successfully', () => {
         const msg = 'Hello World';
-        const wrapper = render(<FirstApp title={msg}/>);
-        expect(wrapper.getByText(msg)).toBeInTheDocument();
+        const wrapper = shallow(<FirstApp title={msg}/>);
+        expect(wrapper).toMatchSnapshot();
     })
 })
