114ee33498fecfbb32ffb0bc7294bd57e35386ea
franciscomarin98 <francmarinc@gmail.com>
2021-11-15 09:53:15 -0500

    :white_check_mark:  feat: React Testing
    End :)

--- src/tests/CounterApp.test.tsx
+++ src/tests/CounterApp.test.tsx
@@ -3,16 +3,33 @@ import {shallow} from 'enzyme';
 
 describe('Tests in <CounterApp/>', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp/>);
+    });
+
     test('should show the CounterApp component successfully', () => {
-        const wrapper = shallow(<CounterApp/>);
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('Should show an HTML <h2> element with the value of counter = 100', () => {
+    test('should show an HTML <h2> element with the value of counter = 100', () => {
         const propValue = 100;
         const wrapper = shallow(<CounterApp value={propValue}/>);
         const contentFound = wrapper.find('h2').text().trim();
         expect(contentFound).toBe(propValue.toString());
+    });
+
+    test('should increase the value of the state by 1.', () => {
+        wrapper.find('button').at(0).simulate('click');
+        const contentFound = wrapper.find('h2').text().trim();
+        expect(contentFound).toBe('11');
+    });
+
+    test('should decrement the value of the state by 1.', () => {
+        wrapper.find('button').at(2).simulate('click');
+        const contentFound = wrapper.find('h2').text().trim();
+        expect(contentFound).toBe('9');
     });
 
 })
